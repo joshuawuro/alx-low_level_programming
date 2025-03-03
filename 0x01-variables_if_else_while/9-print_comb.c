1a2cd30cfcf40a003a1e200dba6cd220b6ed66ec
Joshuawuro <joshuawuro@gmail.com>
2025-03-03 23:38:40 +0000

    print all posible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,16 +6,20 @@
  */
 int main(void)
 {
-	int c;
+	int i,j;
 
-	for (c = '0'; c <= '9'; c++)
+	for (i = 0; i <= 10; i++)
 	{
-		putchar(c);
-		if (c < '9')
+		for (j = i + 1; j <= 10; j++)
 		{
-			putchar(',');
-			putchar(' ');
-		}
+			putchar(i + '0');
+			putchar(j + '0');
+			if(i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}	
 	}
 	putchar('\n');
 	return (0);
