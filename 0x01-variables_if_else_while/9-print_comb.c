94028a41a685caeb7c6ff29ba578af2b0c1fa361
Joshuawuro <joshuawuro@gmail.com>
2025-03-03 23:57:18 +0000

    prints all single numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,9 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = '0'; i < '9'; i++)
 	{
 		putchar(i);
-		if(i < 9)
-		{
-			putchar(',');
-		}
-		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
